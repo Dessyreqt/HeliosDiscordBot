c494c584873bc9e9c1524ccd48b365e03d2b78eb
David Carroll <carroll.david.s@gmail.com>
2021-03-29 12:31:56 -0500

    Add Convert from DateTime to Julian Date

--- HeliosDiscordBot/Domain/JulianDate.cs
+++ HeliosDiscordBot/Domain/JulianDate.cs
@@ -6,6 +6,8 @@
     {
         private static DateTime _julianPeriodStart = new DateTime(-4713, 01, 01);
 
+        public static double Convert(DateTime dateTime) => Convert(dateTime.Year, dateTime.Month, dateTime.Day, dateTime.Hour, dateTime.Minute, dateTime.Second);
+
         public static double Convert(int year, int month, int day, int hour, int minute, int second)
         {
             // (1461 * (Y + 4800 + (M - 14)/12))/4 +(367 * (M - 2 - 12 * ((M - 14)/12)))/12 - (3 * ((Y + 4900 + (M - 14)/12)/100))/4 + D - 32075 
