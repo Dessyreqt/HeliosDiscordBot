3f3db92be018b4882cf9143111d6bd41a49fd37f
David Carroll <carroll.david.s@gmail.com>
2021-04-03 02:29:39 -0500

    Connect to Discord

--- HeliosDiscordBot/Worker.cs
+++ HeliosDiscordBot/Worker.cs
@@ -3,17 +3,26 @@ namespace HeliosDiscordBot
     using System;
     using System.Threading;
     using System.Threading.Tasks;
+    using Discord;
+    using Discord.WebSocket;
     using HeliosDiscordBot.Domain;
+    using HeliosDiscordBot.Settings;
     using Microsoft.Extensions.Hosting;
     using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Options;
 
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly DiscordSettings _discordSettings;
+        private readonly DiscordSocketClient _client;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IOptions<DiscordSettings> discordSettings)
         {
             _logger = logger;
+            _discordSettings = discordSettings.Value;
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -25,10 +34,20 @@ namespace HeliosDiscordBot
             _logger.LogInformation("Sunrise today is at {sunriseTime}", solarCalculation.Sunrise.ToLocalTime());
             _logger.LogInformation("Sunset today is at {sunsetTime}", solarCalculation.Sunset.ToLocalTime());
 
+            _logger.LogInformation("Logging into Discord...");
+            await _client.LoginAsync(TokenType.Bot, _discordSettings.Token);
+            await _client.StartAsync();
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 await Task.Delay(1000, stoppingToken);
             }
+        }
+
+        private Task Log(LogMessage message)
+        {
+            _logger.LogInformation(message.ToString(prependTimestamp: false));
+            return Task.CompletedTask;
         }
     }
 }
