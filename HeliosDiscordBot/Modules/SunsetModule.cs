0de529d722f8d2ad435b610374b9114034b4bdb9
David Carroll <carroll.david.s@gmail.com>
2021-04-06 02:46:43 -0500

    Sunset command

--- HeliosDiscordBot/Modules/SunsetModule.cs
+++ HeliosDiscordBot/Modules/SunsetModule.cs
@@ -6,22 +6,22 @@
     using HeliosDiscordBot.Domain;
     using HeliosDiscordBot.Repository;
 
-    public class SunriseModule : ModuleBase<SocketCommandContext>
+    public class SunsetModule : ModuleBase<SocketCommandContext>
     {
         private readonly IDatabaseRepository _repo;
 
-        public SunriseModule(IDatabaseRepository repo)
+        public SunsetModule(IDatabaseRepository repo)
         {
             _repo = repo;
         }
 
-        [Command("sunrise")]
-        [Summary("Tells you your next sunrise")]
-        public async Task SunriseAsync()
+        [Command("sunset")]
+        [Summary("Tells you your next sunset")]
+        public async Task SunsetAsync()
         {
             var channelId = Context.Channel.Id;
             var notification = await _repo.GetNotificationByChannelIdAsync(channelId);
-            
+
             if (notification == null)
             {
                 await ReplyAsync("You need to set a location first by using `!setlocation <latitude> <longitude>`. For example, `!setlocation 37.8199286 -122.4795565`.");
@@ -31,17 +31,17 @@
             var currentDate = DateTime.UtcNow;
             var date = DateTime.UtcNow.Date.AddHours(12).AddDays(-1);
             var calculation = new SolarCalculation(notification.Latitude, notification.Longitude, date);
-            
-            while (calculation.Sunrise < currentDate)
+
+            while (calculation.Sunset < currentDate)
             {
                 date = date.AddDays(1);
                 calculation = new SolarCalculation(notification.Latitude, notification.Longitude, date);
             }
 
-            var convertedDate = calculation.Sunrise.ToTimeZone(notification.Timezone);
-            var minutesToSunset = (int)Math.Round((calculation.Sunrise - currentDate).TotalMinutes);
+            var convertedDate = calculation.Sunset.ToTimeZone(notification.Timezone);
+            var minutesToSunset = (int)Math.Round((calculation.Sunset - currentDate).TotalMinutes);
 
-            await ReplyAsync($"Your next sunrise is at {convertedDate:h:mm:ss tt}, which is about {minutesToSunset} minutes from now.");
+            await ReplyAsync($"Your next sunset is at {convertedDate:h:mm:ss tt}, which is about {minutesToSunset} minutes from now.");
         }
     }
 }
