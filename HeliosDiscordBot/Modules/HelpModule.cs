2efafaa15e8c0ad6ade93f6f3cfbac50510782ce
David Carroll <carroll.david.s@gmail.com>
2021-04-06 00:51:44 -0500

    Rewrite help

--- HeliosDiscordBot/Modules/HelpModule.cs
+++ HeliosDiscordBot/Modules/HelpModule.cs
@@ -15,16 +15,17 @@ In order for me to help you, I'll need to know a few things. First off, I'll nee
 
 Second, I'll need to know when you want me to notify you. I'll notify you a number of minutes before sunrise or sunset, depending on what you tell me. Use `!notify sunrise 10` to tell me to notify you 10 minutes before sunrise, and use `!notify sunset 30` to tell me to notify you 30 minutes before sunset. I'll only keep track of one notification for sunrise and one for sunset. You can tell me to stop notifying you of sunrise by using `!stopnotify sunrise` and to stop notifying you of sunset by using `!stopnotify sunset`.
 
-Finally, if you want me to convert times to your timezone, I can do that provided you tell me what it is. Please use `!listtimezones` to list all the timezones I know about. Then use `!settimezone (UTC+08:00) Perth` to set your timezone to W. Australia Standard/Daylight Time, as an example.
+Finally, if you want me to convert times to your timezone, I can do that provided you tell me what it is. Please use `!listtimezones` to list all the timezones I know about. Then use `!settimezone (UTC+08:00) Perth` to set your timezone to W. Australia Standard/Daylight Time, as an example.");
 
-In summary:
-`!setlocation 37.8199286 -122.4795565` - sets your location of interest to the Golden Gate Bridge. If you're interested in getting your location try using https://www.gps-coordinates.net/my-location
-`!notify sunrise 10` - will start notifying 10 minutes before sunrise
-`!notify sunset 30` - will start notifying 30 minutes before sunset
+            await ReplyAsync(@"In summary:
+
+`!setlocation <latitude> <logitude>` - (Example: `!setlocation 37.8199286 -122.4795565`) - sets your location of interest to the Golden Gate Bridge. If you're interested in getting your location try using https://www.gps-coordinates.net/my-location
+`!notify sunrise <minutes>` - (Example: `!notify sunrise 10`) - will start notifying 10 minutes before sunrise
+`!notify sunset <minutes>` - (Example: `!notify sunset 30`) - will start notifying 30 minutes before sunset
 `!stopnotify sunrise` - will stop notifying about sunrise
 `!stopnotify sunset` - will stop notifying about sunset
 `!listtimezones` - will list the time zones I know about
-`!settimezone (UTC+08:00) Perth` - will set your timezone to W. Australia Standard/Daylight Time");
+`!settimezone <time zone>` - (Example: `!settimezone (UTC+08:00) Perth`) - will set your timezone to W. Australia Standard/Daylight Time");
         }
     }
 }
