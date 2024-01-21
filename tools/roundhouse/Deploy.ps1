343a0ea16eabc4dbb9c99313f6b10e1851d43986
David Carroll <carroll.david.s@gmail.com>
2024-01-21 01:45:34 -0600

    More changes to deploy db script

--- tools/roundhouse/Deploy.ps1
+++ tools/roundhouse/Deploy.ps1
@@ -2,5 +2,5 @@ $roundhouse_exe_path = ".\rh.exe"
 $db_dir = ".\scripts"
 $connection_string = $OctopusParameters["DatabaseSettings:ConnectionString"]
 
+Write-Host "Deploying database to $connection_string"
 &$roundhouse_exe_path /c=$connection_string /f=$db_dir /cds="$db_dir\create.sql" /silent /transaction
-
